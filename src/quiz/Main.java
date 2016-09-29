53cebe7f668a01596f21574eaf93fe9ae85356ab
Deven Bawale <dbawale@pdx.edu>
2016-09-28 22:07:38 -0700

    Added the answers class

--- src/quiz/Main.java
+++ src/quiz/Main.java
@@ -1,8 +1,16 @@
 package quiz;
 
+import javafx.util.Pair;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Answers a = new Answers();
+        a.addAnswer(new Pair("a","answer1"));
+        a.addAnswer(new Pair("b","answer2"));
+        System.out.println(a.toString());
     }
 }
